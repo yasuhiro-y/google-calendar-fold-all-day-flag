e5bf94f110a1256fb6a9a4118b3d81dcb6810091
yasuhiro-y <yasuhiro.yokota@my-best.com>
2022-10-06 14:00:07 +0900

    first fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { SheetService } from './sheet.service';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare let global: any;
-
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
-};
+function getAllDayEvents() {
+  const calendar = CalendarApp.getDefaultCalendar();
+  const today = new Date();
+  const todayEvents = calendar.getEventsForDay(today);
+  const startTime = new Date();
+  startTime.setHours(10);
+  startTime.setMinutes(0);
+  const endTime = new Date();
+  endTime.setHours(11);
+  endTime.setMinutes(0);
+  for (let i = 0; i < todayEvents.length; i++) {
+    const e = todayEvents[i];
+    e.isAllDayEvent() && e.setTime(startTime, endTime);
+  }
+  return;
+}
